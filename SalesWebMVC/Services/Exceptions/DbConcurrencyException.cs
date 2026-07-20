e9f3ac570d61c0bc3411aacf08cc53d7a9e68bbf
Lorenzo Ronzani <lorenzo.lronzani@gmail.com>
2026-07-20 11:33:21 -0300

    Update seller and custom service exception

---
+++ SalesWebMVC/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbConcurrencyException : Exception
+    {
+        public DbConcurrencyException(string message) : base(message) { } 
+    }
+}
